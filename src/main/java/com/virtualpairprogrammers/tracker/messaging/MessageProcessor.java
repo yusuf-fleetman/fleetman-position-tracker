932db99f5af6133f521318adfa36dcb9a4de0e34
yusuf_kaya <160890360+yusufkaya01@users.noreply.github.com>
2024-10-31 15:13:01 +0300

    Update MessageProcessor.java
    my change

--- src/main/java/com/virtualpairprogrammers/tracker/messaging/MessageProcessor.java
+++ src/main/java/com/virtualpairprogrammers/tracker/messaging/MessageProcessor.java
@@ -6,6 +6,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
+import java.util.Random; // Import Random class
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
@@ -23,21 +24,29 @@ public class MessageProcessor {
 	
 	private DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
 
+	// Create a Random instance
+	private Random random = new Random();
+
 	@JmsListener(destination="${fleetman.position.queue}")
-	public void processPositionMessageFromQueue(Map<String, String> incomingMessage ) throws ParseException 
-	{
+	public void processPositionMessageFromQueue(Map<String, String> incomingMessage) throws ParseException {
 		String positionDatestamp = incomingMessage.get("time");
 		Date convertedDatestamp = format.parse(positionDatestamp);
 		
+		// Define the speed range
+		BigDecimal minSpeed = new BigDecimal("0.0");  // Minimum speed
+		BigDecimal maxSpeed = new BigDecimal("100.0"); // Maximum speed
+
+		// Generate a random speed within the range
+		BigDecimal randomSpeed = minSpeed.add(maxSpeed.subtract(minSpeed).multiply(BigDecimal.valueOf(random.nextDouble())));
+
 		VehiclePosition newReport = new VehicleBuilder()
 				                          .withName(incomingMessage.get("vehicle"))
 				                          .withLat(new BigDecimal(incomingMessage.get("lat")))
 				                          .withLng(new BigDecimal(incomingMessage.get("long")))
 				                          .withTimestamp(convertedDatestamp)
-			                                  .withSpeed(new BigDecimal ("47.5"))
+				                          .withSpeed(randomSpeed) // Use the generated random speed
 				                          .build();
-				                          
+				                          	
 		data.updatePosition(newReport);
 	}
-
 }
